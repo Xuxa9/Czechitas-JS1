e785f6bffeeefa799a35966ff0deb5e9c001bb62
Zuzana Čechová <zu.cechova9@gmail.com>
2024-09-30 20:25:26 +0200

    novy

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let prijem2 = (0.6 * 174) * 12 + studenti * listek65
 
 document.body.innerHTML +=  "<p>" + prijem2 + "</p>"
  
-let prijem3 = 0.60 * prijem + studenti * listek65
+let prijem3 = 0.60 * prijem + studenti * listek65 *15
 
 document.body.innerHTML +=  "<p>" + prijem3 + "</p>"
 
