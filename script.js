bfe0f50abf842671bcf3ccdd1283985e088b50df
Zuzana Čechová <zu.cechova9@gmail.com>
2024-09-30 20:27:58 +0200

    prijem se studenty

--- script.js
+++ script.js
@@ -5,11 +5,8 @@ document.body.innerHTML +=  "<p>" + prijem + "</p>"
 
 const studenti = 0.4 * 174
 const listek65 = listek * 0.65
-let prijem2 = (0.6 * 174) * 12 + studenti * listek65
+ 
+let prijem2 = 0.60 * prijem + studenti * listek65 *15
 
 document.body.innerHTML +=  "<p>" + prijem2 + "</p>"
- 
-let prijem3 = 0.60 * prijem + studenti * listek65 *15
-
-document.body.innerHTML +=  "<p>" + prijem3 + "</p>"
 
