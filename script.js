52de7c6599a869ab6e1e19e7ea9a28f061697a48
Zuzana Čechová <zu.cechova9@gmail.com>
2024-10-05 19:45:26 +0200

    ukol jedna kompletni

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const listek = 12
 let  prijem = listek * 174 * 15
 
-document.body.innerHTML +=  "<p>" + prijem + "</p>"
+document.body.innerHTML +=  "<p>" + "Příjem divadla: " + prijem + " Kč" + "</p>"
 
 const studenti = 0.4 * 174
 const listek65 = listek * 0.65
  
 let prijem2 = 0.60 * prijem + studenti * listek65 *15
 
-document.body.innerHTML +=  "<p>" + prijem2 + "</p>"
+document.body.innerHTML +=  "Příjem divadla se studenty: " + prijem2 + " Kč" + "</p>"
 
